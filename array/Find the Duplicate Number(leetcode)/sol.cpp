d31456c1a284697f72ccba8abd5e20989b6db354
kv82394 <kv82394@gmail.com>
2021-01-13 09:51:44 +0530

    Create sol.cpp

---
+++ array/Find the Duplicate Number(leetcode)/sol.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int arr[n];
+        for (int i = 0 ;i< n ;i++){arr[i]=0;}
+        for (int i = 0 ;i< n ;i++){
+            arr[nums[i]-1]++;
+        }
+        for (int i = 0 ;i< n ;i++){
+            if (arr[i]>1)return i+1;
+        }
+        
+        return 1;
+        
+    }
+};
