ab59c08d8981352ba1bdf211351cc49132b4f92d
kv82394 <kv82394@gmail.com>
2021-01-27 09:57:40 +0530

    Create sol.cpp

---
+++ array/reverse the array/sol.cpp
@@ -0,0 +1,41 @@
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    
+  int n = str.length();
+  int i=0;
+  int j = n-1;
+  while(i<=j){
+      swap(str[i],str[j]);
+      i++;j--;
+  }
+  return str;
+}
