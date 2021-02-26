5279ef90c0d5e1c17f8b940026729d7ac8086018
kv82394 <kv82394@gmail.com>
2021-02-26 11:41:07 +0530

    Create sol.cpp

---
+++ important algorithms/merg sort/sol.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merg(int arr[],int start,int mid,int end){
+    vector<int> vec;
+    int i = start;int j = mid+1;
+    while(i<=mid && j<=end){
+        if (arr[i]<arr[j]){
+            vec.push_back(arr[i]);
+            i++;
+        }else {
+            vec.push_back(arr[j]);
+            j++;           
+        }
+    }
+    while(i<=mid){
+            vec.push_back(arr[i]);
+            i++;      
+    }
+    while(j<=end){
+            vec.push_back(arr[j]);
+            j++;      
+    }
+    int k = 0;
+    for (i = start;i<=end;i++){
+        arr[i] = vec[k++];
+    }
+    
+}
+void fun(int arr[],int start,int end){
+    if (start<end){
+        int mid = (start+end)/2;
+        fun(arr,start,mid);
+        fun(arr,mid+1,end);
+        merg(arr,start,mid,end);
+    }
+}
+void mergeSort(int input[], int size){
+    fun(input,0,size-1);
+    
+    
+}
