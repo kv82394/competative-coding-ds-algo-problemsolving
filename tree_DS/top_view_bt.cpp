4d26d8db4fe8f0aa9f255227e7ccfae00ceb3bb4
kv82394 <kv82394@gmail.com>
2022-01-30 22:15:34 +0530

    Create top_view_bt.cpp

---
+++ tree_DS/top_view_bt.cpp
@@ -0,0 +1,168 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+
+
+/*
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        if (root==NULL)return ans;
+        map<int,int> mp;
+        queue<pair<int,Node*>> st;
+        pair<int,Node*> temp_pair;
+        temp_pair.first = 0;
+        temp_pair.second = root;
+        st.push(temp_pair);
+        while(!st.empty()){
+            temp_pair = st.front();
+            st.pop();
+            int nodeindex = temp_pair.first;
+            Node *node = temp_pair.second;
+            if(mp[nodeindex]==0){
+                mp[nodeindex] = node->data;
+            }
+             if (node->right!=NULL){
+             temp_pair.first = nodeindex+1;
+             temp_pair.second = node->right;
+             //cout<<"xyz  "<<temp_pair.first<<"  "<<temp_pair.second->data<<endl;
+             st.push(temp_pair);
+            }
+            if (node->left!=NULL){
+             temp_pair.first = nodeindex-1;
+             temp_pair.second = node->left;
+             st.push(temp_pair);
+            }
+             
+
+            
+        }
+        for (auto it = mp.begin();it!=mp.end();it++){
+            ans.push_back(it->second);
+        }
+        return ans;
+        
+        
+    }
+
+};
+
+
+
+// { Driver Code Starts.
+
+int main() {
+    int tc;
+    cin>>tc;
+    cin.ignore(256, '\n');
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        vector<int> vec = ob.topView(root);
+        for(int x : vec)
+            cout<<x<<" ";
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
